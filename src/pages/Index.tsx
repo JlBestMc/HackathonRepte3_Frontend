9c4a5c4b05598fe748838a8874f06e6f9e1d029b
JlBestMc <jlrcmt@gmail.com>
2025-10-15 13:09:39 +0200

    welcomePage started

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,15 @@
 import Main from '@layouts/authn/Main'
 
 const Index = () => {
-    return <Main>A form</Main>
+    return (
+        <Main>
+            <div
+                className={`w-full h-full transition-opacity duration-800 ease-out"
+        }`}
+                style={{ clipPath: 'inset(0 0 60px 0)' }}
+            ></div>
+        </Main>
+    )
 }
 
 export default Index
