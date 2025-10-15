6d9dcfdfe64f595c5a43fe8a1fe7317c36a415f3
antoniofelices <antonio@studiomoare.com>
2025-10-15 13:42:04 +0200

    Add form sign in, form sign up

--- src/components/FormSignUp.tsx
+++ src/components/FormSignUp.tsx
@@ -1,23 +1,12 @@
-import { toast } from 'sonner'
-import {
-    IdCard,
-    Lock,
-    LockKeyhole,
-    Mail,
-    User,
-    UserRoundCog,
-} from 'lucide-react'
+// import { toast } from 'sonner'
+import { Lock, LockKeyhole, Mail, User } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { createUser } from '@/services/supabaseAdmin'
-import { USERROLES } from '@/config/config.ts'
-import { useLogger } from '@/hooks/useLogger'
 import { Button } from '@/components/ui/base/button'
 import { Form } from '@components/ui/base/form'
 import FormFieldInput from '@/components/ui/FormFieldInput'
-import FormFieldSelect from '@/components/ui/FormFieldSelect'
-import content from '@/config/data/user/registerForm'
+import content from '@/config/data/formSignUp'
 
 const registerUserSchema = z
     .object({
@@ -29,16 +18,10 @@ const registerUserSchema = z
             .string()
             .min(3, content.errorUserLastNameTooShort)
             .max(20, content.errorUserLastNameTooLong),
-        dni: z
-            .string()
-            .min(9, content.errorUserDniTooShort)
-            .max(9, content.errorUserDniTooLong)
-            .regex(/^\d{8}[A-Z]$/, content.errorUserDniInvalidFormat),
         email: z
             .string()
             .email(content.errorEmailInvalid)
             .min(1, content.errorEmailRequired),
-        role: z.enum(USERROLES),
         password: z
             .string()
             .min(8, content.errorPasswordTooShort)
@@ -56,7 +39,7 @@ const registerUserSchema = z
 type FormData = z.infer<typeof registerUserSchema>
 
 const RegisterUserForm = () => {
-    const { logError, logSuccess } = useLogger('RegisterUserForm')
+    // const { logError, logSuccess } = useLogger('RegisterUserForm')
 
     const defaultValues = {
         email: '',
@@ -64,7 +47,6 @@ const RegisterUserForm = () => {
         confirmPassword: '',
         userName: '',
         userLastName: '',
-        dni: '',
         role: 'user' as const,
     }
 
@@ -73,36 +55,37 @@ const RegisterUserForm = () => {
         defaultValues: defaultValues,
     })
 
-    const onSubmit = async (formData: FormData) => {
-        try {
-            const newUser = await createUser({
-                email: formData.email,
-                password: formData.password,
-                user_name: formData.userName,
-                user_last_name: formData.userLastName,
-                dni: formData.dni,
-                role: formData.role,
-            })
-            toast.success(content.textToastSuccess)
-            logSuccess(content.textToastSuccess, content.title)
-            form.reset()
-            return newUser
-        } catch (error) {
-            logError(content.textToastFail, error, content.title)
-            toast.error(content.textToastFail)
-            return
-        }
-    }
+    // const onSubmit = async (formData: FormData) => {
+    //     try {
+    //         const newUser = await createUser({
+    //             email: formData.email,
+    //             password: formData.password,
+    //             user_name: formData.userName,
+    //             user_last_name: formData.userLastName,
+    //             dni: formData.dni,
+    //             role: formData.role,
+    //         })
+    //         toast.success(content.textToastSuccess)
+    //         logSuccess(content.textToastSuccess, content.title)
+    //         form.reset()
+    //         return newUser
+    //     } catch (error) {
+    //         logError(content.textToastFail, error, content.title)
+    //         toast.error(content.textToastFail)
+    //         return
+    //     }
+    // }
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
+            {/* <form onSubmit={form.handleSubmit(onSubmit)}> */}
+            <form>
                 <FormFieldInput
                     control={form.control}
                     fieldName="userName"
                     icon={User}
                     label={content.labelUserName}
-                    placeholder="Manolo"
+                    placeholder="Michael"
                     type="text"
                 />
                 <FormFieldInput
@@ -110,7 +93,7 @@ const RegisterUserForm = () => {
                     fieldName="userLastName"
                     icon={User}
                     label={content.labelUserLastName}
-                    placeholder="Kabezabolo"
+                    placeholder="Barbaro"
                     type="text"
                 />
                 <FormFieldInput
@@ -118,16 +101,8 @@ const RegisterUserForm = () => {
                     fieldName="email"
                     icon={Mail}
                     label={content.labelEmail}
-                    placeholder="nf@manolo.es"
+                    placeholder="mb@nyt.com"
                     type="email"
-                />
-                <FormFieldInput
-                    control={form.control}
-                    fieldName="dni"
-                    icon={IdCard}
-                    label={content.labelDNI}
-                    placeholder="12121212P"
-                    type="text"
                 />
                 <FormFieldInput
                     control={form.control}
@@ -142,14 +117,6 @@ const RegisterUserForm = () => {
                     icon={LockKeyhole}
                     label={content.labelConfirmPassword}
                     type="password"
-                />
-                <FormFieldSelect
-                    control={form.control}
-                    fieldName="role"
-                    icon={UserRoundCog}
-                    label={content.labelSelectRole}
-                    options={USERROLES}
-                    placeholder="User"
                 />
                 <Button type="submit" className="w-full">
                     {form.formState.isSubmitting
