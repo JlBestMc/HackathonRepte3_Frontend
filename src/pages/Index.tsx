61e41ccc06dc1335197bcaaade82247ff13f02b6
JlBestMc <jlrcmt@gmail.com>
2025-10-15 13:14:40 +0200

    spline added

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import Main from '@layouts/authn/Main'
+import Spline from '@splinetool/react-spline'
 
 const Index = () => {
     return (
@@ -7,7 +8,9 @@ const Index = () => {
                 className={`w-full h-full transition-opacity duration-800 ease-out"
         }`}
                 style={{ clipPath: 'inset(0 0 60px 0)' }}
-            ></div>
+            >
+                <Spline scene="https://prod.spline.design/jXRW7HLwo1u3g2LU/scene.splinecode" />
+            </div>
         </Main>
     )
 }
