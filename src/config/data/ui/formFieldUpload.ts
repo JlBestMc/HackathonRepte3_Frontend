2db9adeee01799469d7fd2404c5b6a9b6b0452a7
JlBestMc <jlrcmt@gmail.com>
2025-10-17 10:09:02 +0200

    new components updated

--- src/config/data/ui/formFieldUpload.ts
+++ src/config/data/ui/formFieldUpload.ts
@@ -1,4 +1,6 @@
 export default {
   textSelectFile: 'Selecciona un archivo',
   textFileSize: 'El tamaño máximo es',
+  textAcceptedType: 'Tipos permitidos:',
+  textMaxFileSize: 'Tamaño máximo',
 }
--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,5 @@
-import { Link } from '@tanstack/react-router'
 // import { useAuth } from '@/hooks/useAuth'
 import Main from '@layouts/authn/Main'
-import { Button } from '@/components/ui/base/button'
 // import content from '@/config/data/pages/error'
 // import pictureColleDelleFinestre from '@/assets/images/error404-finestre.webp'
 
